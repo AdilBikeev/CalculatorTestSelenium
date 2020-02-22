b6f7c99f8998d50a0be838b86c5119bffa7b4a15
AdilBikeev <bikeev1999@mail.ru>
2020-02-22 20:33:03 +0300

    develop-test Добавли тесты для проверки наличия калькулятора на сайте и кнопок операций на нем

--- CalculatorTestSelenium/UnitTest1.cs
+++ CalculatorTestSelenium/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -32,6 +33,51 @@ namespace CalculatorTestSelenium
         public void TearDown()
         {
             firefox.Quit();
+        }
+
+        [TestMethod]
+        [Description("Проверяет есть ли на сайте калькулятор")]
+        public void CalculatorFind()
+        {
+            Assert.IsNotNull(firefox.FindElementById("korpus_calulator"));
+        }
+
+        [TestMethod]
+        [Description("Проверяет наличие всех нужных кнопок операций")]
+        [DataRow("PLS", "+")]
+        [DataRow("MIN", "-")]
+        [DataRow("UMN", "\u00d7")]
+        [DataRow("DLT", "\u00f7")]
+        [DataRow("RAV", "=")]
+        [DataRow("PII", "\u03c0")]
+        [DataRow("EEE", "e")]
+        [DataRow("SSL", "(")]
+        [DataRow("SSP", ")")]
+        [DataRow("KKV", "\u221a")]
+        [DataRow("XKV", "^")]
+        [DataRow("LGN", "ln")]
+        [DataRow("LGG", "lg")]
+        [DataRow("ABC", "Abc")]
+        [DataRow("SIN", "Sin")]
+        [DataRow("ASN", "aSin")]
+        [DataRow("COS", "Cos")]
+        [DataRow("ACS", "aCos")]
+        [DataRow("TGG", "Tg")]
+        [DataRow("ATG", "atg")]
+        [DataRow("CTG", "cTg")]
+        [DataRow("XSY", "^")]
+        [DataRow("XZY", "\u221a")]
+        [DataRow("LGY", "log")]
+        [DataRow("ZPT", ",")]
+        public void CheckKeysOperation(string key, object value)
+        {
+            var obj = firefox.ExecuteScript("return llliilllll");
+            Assert.IsInstanceOfType(obj.GetType(), new Dictionary<string, object>().GetType());
+
+            var dictKeysOperation = (Dictionary<string,object>)obj;
+
+            Assert.IsTrue(dictKeysOperation.ContainsKey(key));
+            Assert.IsTrue(dictKeysOperation[key] == value);
         }
     }
 }
