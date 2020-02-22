6371f9da07eafaeaeacc3a71521bffaec11f64cc
AdilBikeev <bikeev1999@mail.ru>
2020-02-22 17:22:21 +0300

    master Создал проект

---
+++ CalculatorTestSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTestSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTestSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a28d763f-f3b7-4b82-8ff8-4a86f0ee30d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorTestSelenium/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace CalculatorTestSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Адрес сайта с калькулятором
+        /// </summary>
+        const string url = "https://calculator888.ru/";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
